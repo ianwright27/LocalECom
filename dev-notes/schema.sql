8e4f47a036ff068f6024908dfd677a7190e8e55c
ianwright27 <thewian27@gmail.com>
2026-02-06 18:11:06 +0300

    log_dir

--- dev-notes/schema.sql
+++ dev-notes/schema.sql
@@ -45,7 +45,7 @@ CREATE TABLE customers (
     id INT PRIMARY KEY AUTO_INCREMENT,
     business_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
     address TEXT,
